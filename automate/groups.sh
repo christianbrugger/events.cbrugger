94cf1f97d2c51be9d6c72cbf6a108fc0d80b662d
Christian Brugger <christian.brugger.ext@siemens.com>
2019-08-04 01:58:31 +0200

    added requriments && pushing

--- automate/groups.sh
+++ automate/groups.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 set -e
-
 . automate/helpers.sh
 
 export FILE_TAG=`TZ="Europe/Berlin" date +"%Y_%m_%d__%H_%M"`
 
+# install requirements
+python -m pip -r requirements.txt
+
+# run script
 python scripts/get_groups.py --headless --chunks=50
 
-cat ${FILE_TAG}_groups0.txt
+# push results to next repository
+export FILENAME=${FILE_TAG}_groups0.txt
+git clone https://github.com/christianbrugger/events.cbrugger.events0.git
+cd events.cbrugger.events0
+mv ../${FILENAME} inputs
+git add inputs/${FILENAME}
+git commit --message "${FILENAME}"
+
+setup_git
+upload_files
